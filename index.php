489a5e1afeddd5a263147dc949f80a25f7ad313c
R. A. Souza <rodrigo.alves@glossybox.com.br>
2013-08-16 22:18:10 -0300

    NEW: Harvest all E-mails that opened some NL within 15 days

--- index.php
+++ index.php
@@ -6,10 +6,8 @@ try
 	$obj = new SFTPSpider();
 	$obj->setLogging(true);
 	$obj->setDateFormat('Ymd');
-	$obj->addFile('UNSUBSCRIBES.csv');
-	$obj->addFile('BOUNCES.csv');
-	$obj->addFile('COMPLAINTS.csv');
-	$obj->init('blacklist.csv');
+	$obj->addFile('OPENS.csv');
+	$obj->init('opens.csv');
 } catch(Exception $e) {
 	echo "Error", $e->getMessage();
 }
